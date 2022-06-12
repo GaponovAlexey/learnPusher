5d8b2a481865a9c08d6af40e7b46d97a7aededd0
kekelberi <alexei.gaponov@gmail.com>
2022-06-12 20:05:01 +0300

    save

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+      <script src="https://js.pusher.com/7.1/pusher.min.js"/>
       <link
           href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css'
           rel='stylesheet'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,44 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import s from '../styles/Home.module.css'
+import Pusher from 'pusher-js'
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
+  const [username, setUsername] = useState('')
+
+  const [messages, setMessages] = useState([])
+  const [message, setMessage] = useState('')
+  let allMessages = [] as any
+
+  const submit = async (e: any) => {
+    e.preventDefault()
+    await fetch('http://localhost:3000/api/message', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        message,
+      }),
+    })
+    setMessage('')
+  }
+
+  useEffect(() => {
+    Pusher.logToConsole = true
+
+    const pusher = new Pusher('c5b6b19d7f78dc9bbd77', {
+      cluster: 'eu',
+    })
+
+    const channel = pusher.subscribe('chat')
+    channel.bind('message', function (data: any) {
+      allMessages.push(data)
+      setMessages(allMessages)
+    })
+  }, [])
+
   return (
     <div>
       <Head>
@@ -10,30 +46,38 @@ const Home: NextPage = () => {
       </Head>
       <div>
         <div className='container d-flex flex-column align-items-stretch flex-shrink-0 bg-white'>
-          <a
-            href='/'
-            className='d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom'
-          >
-            <svg className='bi pe-none me-2' width='30' height='24'>
-              <use href='#bootstrap'></use>
-            </svg>
-            <span className='fs-5 fw-semibold'>List group</span>
-          </a>
-          <div className='list-group list-group-flush border-bottom scrollarea'>
-            <a
-              href='#'
-              className='list-group-item list-group-item-action py-3 lh-sm'
-            >
-              <div className='d-flex w-100 align-items-center justify-content-between'>
-                <strong className='mb-1'>List group item heading</strong>
-                <small className='text-muted'>Tues</small>
-              </div>
-              <div className='col-10 mb-1 small'>
-                Some placeholder content in a paragraph below the heading and
-                date.
-              </div>
-            </a>
+          <div className='d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom'>
+            <svg className='bi pe-none me-2' width='30' height='24'></svg>
+            <input
+              value={username}
+              placeholder='start typing...'
+              onChange={(e) => setUsername(e.target.value)}
+              className='fs-5 fw-semibold'
+            />
           </div>
+          <div
+            style={{ minHeight: '300px' }}
+            className='list-group list-group-flush border-bottom scrollarea'
+          >
+            {messages.map((message) => {
+              return (
+                <div className='list-group-item list-group-item-action py-3 lh-sm'>
+                  <div className='d-flex w-100 align-items-center justify-content-between'>
+                    <strong className='mb-1'>{message.username}</strong>
+                  </div>
+                  <div className='col-10 mb-1 small'>{message.message}</div>
+                </div>
+              )
+            })}
+          </div>
+          <form onSubmit={submit}>
+            <input
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className='form-control'
+              placeholder='Write a message'
+            />
+          </form>
         </div>
       </div>
     </div>
